d6eb1f04bb19ac1f8dd52202abb1cf3ce514f0b6
Felix Favour <felixfavour@users.noreply.github.com>
2019-12-08 19:54:02 +0100

    Database was linked to ui 2

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(author=User.objects.get(username='chinemerem')).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
